0bad6698f32896238ea8872db05111e3ce076bf3
mhyuan <dsz9258@163.com>
2018-11-04 22:21:12 +0800

    我的积分页面

--- src/pages/personal/index.js
+++ src/pages/personal/index.js
@@ -1,20 +1,46 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import Footer from '../../components/footer';
-import {
-    getPersonalData,
-} from '../../store/actionCreators';
+import { getPersonalData } from '../../store/actionCreators';
 import {
     PersionalWrap,
+    TopContent,
+    MainContent,
 } from './style';
 
 class Personal extends PureComponent {
     render() {
+        let { personal_info } = this.props;
+        const info = personal_info.toJS();
+console.log('info', info);
+
         return (
             <PersionalWrap>
-                <div>
-                    Personal
-                </div>
+                <TopContent>
+                    <div className="top_wrap">
+                        <h3 className="total_integral">总积分</h3>
+                        <span className="integral">{info.integral}</span>
+                        <span className="integral_level">等级: {info.integ_level}</span>
+                        <button onClick={this.exchange} className="exchange_button">积分兑换</button>
+                    </div>
+                </TopContent>
+                <MainContent>
+                    <div className="item">
+                        <span>积分说明</span>
+                    </div>
+                    <div className="item">
+                        <span>我的报名</span>
+                    </div>
+                    <div className="item">
+                        <span>个人信息</span>
+                    </div>
+                    <div className="item">
+                        <span>邀请好友</span>
+                    </div>
+                    <div className="item">
+                        <span>在线客服</span>
+                    </div>
+                </MainContent>
                 <Footer current={this.props.match.path}></Footer>
             </PersionalWrap>
         )
@@ -24,13 +50,17 @@ class Personal extends PureComponent {
     }
 }
 const mapState = (state) => ({
-    
+    personal_info: state.get('personal_info')
 })
 
 const mapDispatch = (dispatch) => ({
     fetchPersonDeatil() {
         let action = getPersonalData();
         dispatch(action);
+    },
+    exchange() {
+        console.log('exchange integral!!!!!');
+        
     }
 })
 
--- src/pages/personal/style.js
+++ src/pages/personal/style.js
@@ -1,5 +1,83 @@
 import Styled from 'styled-components';
 
 export const PersionalWrap = Styled.div`
+    position: reloative;
+    display: block;
+    box-sizing: border-box;
+`
+export const TopContent = Styled.div`
+    display: block;
+    padding: 0 auto 0.4rem;
+    overflow: hidden;
+    box-sizing: border-box;
+    .top_wrap {
+        display: inline-block;
+        text-align: center;
+        width: 100%;
+        margin-top: 0.3rem;
+        box-sizing: border-box;
+        .total_integral {
+            display: inline-block;
+            width: 100%;
+            height: 0.4rem;
+            font-size: 0.24rem;
+            font-weight: bold;
+            line-height: 0.4rem;
+            text-align: center;
+            box-sizing: border-box;
+        }
+        .integral {
+            display: inline-block;
+            width: 100%;
+            height: 0.6rem;
+            line-height: 0.6rem;
+            font-size: 0.36rem;
+            font-weight: bold;
+            text-align: center;
+            box-sizing: border-box;
+        }
+        .integral_level {
+            display: inline-block;
+            width: 100%;
+            height: 0.4rem;
+            font-size: 0.26rem;
+            font-weight: bold;
+            line-height: 0.4rem;
+            text-align: center;
+            box-sizing: border-box;
+        }
+        .exchange_button {
+            display: inline-block;
+            margin: 0.2rem auto 0.4rem;
+            width: 1.4rem;
+            height: 0.5rem;
+            line-height: 0.5rem;
+            text-align: center;
+            white-space: nowrap;
+            background-color: #f5f5f5;
+            border-radius: 0.1rem;
+            border: 0.01rem solid #666;
+            color: #333;
+            font-size: 0.22rem;
+            outline: none;
+            cursor: pointer;
+            box-sizing: border-box;
+        }
+    }
+`
 
+export const MainContent = Styled.div`
+    display: block;
+    box-sizing: border-box;
+    border-top: 0.01rem solid #ccc;
+    .item {
+        display: inline-block;
+        width: 100%;
+        height: 0.8rem;
+        line-height: 0.8rem;
+        padding: 0 0.4rem;
+        cursor: pointer;
+        box-sizing: border-box;
+        border-bottom: 0.01rem solid #ccc;
+    }
 `
