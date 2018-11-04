9ef9606e5f5e72d993f3f12978720fa022ba0078
mhyuan <dsz9258@163.com>
2018-11-04 23:26:37 +0800

    add header

--- src/pages/activityDetail/index.js
+++ src/pages/activityDetail/index.js
@@ -1,9 +1,15 @@
 import React, { PureComponent }  from 'react';
+import Header from '../../components/header';
 
 class ActivityDetail extends PureComponent {
     render() {
         return (
             <div>
+                <Header>
+                    {
+                        <span show_back="show">活动专区</span>
+                    }
+                </Header>
                 ActivityDetail
             </div>
         )
