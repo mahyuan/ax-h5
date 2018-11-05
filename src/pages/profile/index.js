3cd528fd841c99d2c4ed3b1963a4c5f2efef6611
mhyuan <dsz9258@163.com>
2018-11-05 18:04:18 +0800

    profile page UI finished

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,27 +1,79 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import Header from '../../components/header';
+import { getPersonalData } from '../../store/actionCreators';
 import {
     ProfileWrap,
     ProfileContent,
+    ChangePassward,
+    Logout,
 } from './style';
 
 class Profile extends PureComponent {
     render() {
+        let { personal_info } = this.props;
+        let profile = (personal_info && personal_info.size > 0) ? personal_info.toJS().person_profile : {};
+        console.log('profile' , profile);
+        
         return (
-            <ProfileWrap>
+            profile.uid ? (
+                <ProfileWrap>
                 <Header>
                     {
                         <span show_back="show">个人信息</span>
                     }
                 </Header>
                 <ProfileContent>
-
-                    ProfileWrap
+                    <div className="info_content">
+                        <span className="info_item">{profile.user_name}</span>
+                        <span className="change_button">修改</span>
+                    </div>
+                    <div className="info_content">
+                        <span className="info_item">{profile.mobile}</span>
+                        <span className="change_button">修改</span>
+                    </div>
+                    <div className="info_content">
+                        <span className="info_item">{profile.email}</span>
+                        <span className="change_button">修改</span>
+                    </div>
+                    <div className="info_content">
+                        <span className="info_item">{profile.school}</span>
+                        <span className="change_button">修改</span>
+                    </div>
                 </ProfileContent>
+                <ChangePassward>
+                    <div className="change_passwd_button">修改密码</div>
+                </ChangePassward>
+                <Logout>
+                    <div className="logout_button">退出登录</div>
+                </Logout>
             </ProfileWrap>
+            ) : null
         )
+    }
+    componentDidMount() {
+        console.log('this.props.personal_info', this.props.personal_info);
+        
+        this.props.getProfile(this.props.personal_info);
     }
 }
 
-export default connect()(Profile);
+const mapState = (state) => ({
+    personal_info: state.get('personal_info'),
+})
+
+const mapDispatch = (dispatch) => ({
+    getProfile(profile) {
+        if(!(profile && profile.size > 0)) {
+            let action = getPersonalData();
+            dispatch(action);
+            console.log('1');
+            
+        } else {
+            console.log('2, profile');
+            
+        }
+    }
+})
+
+export default connect(mapState, mapDispatch)(Profile);
--- src/pages/profile/style.js
+++ src/pages/profile/style.js
@@ -6,7 +6,67 @@ export const ProfileWrap = Styled.div`
 export const ProfileContent = Styled.div`
     position: relative;
     display: block;
+    padding: 0 0.2rem;
     margin-top: 0.8rem;
+    .info_content{
+        display: inline-block;
+        height: 0.8rem;
+        width: 100%;
+        line-height: 0.8rem;
+        box-sizing: border-box;
+        border-bottom: 0.01rem solid #eee; 
+        .info_item {
+            display: inline-block;
+            width: 80%;
+        }
+        .change_button {
+            display: inline-block;    
+            width: 20%;
+            text-align: center;
+        }
+    }
+`
+
+export const ChangePassward = Styled.div`
+    position: relative;
+    display: inline-block;
+    width: 100%;
+    margin-top: 0.5rem;
+    padding: 0.2rem auto 0.2rem;
+    box-sizing: border-box;
+    text-align: center;
+    .change_passwd_button {
+        display: inline-block;
+        width: 2rem;
+        height: 0.5rem;
+        margin: 0.2rem 0;
+        text-align: center;
+        box-sizing: border-box;
+        line-height: 0.5rem; 
+        border: 0.01rem solid #999;
+        border-radius: 0.1rem;
+    }
+`
+    
+export const Logout = Styled.div`
+    position: relative;
+    display: inline-block;
+    width: 100%;
+    padding: 0.2rem auto;
+    box-sizing: border-box;
+    text-align: center;
+    .logout_button {
+        display: inline-block;
+        width: 2rem;
+        height: 0.5rem;
+        line-height: 0.5rem;
+        margin: 0.2rem 0;
+        text-align: center;
+        box-sizing: border-box;
+        border: 0.01rem solid #999;
+        border-radius: 0.1rem;
+    }
+
 `
 
 
