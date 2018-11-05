2b20bba7f03bed65db3e76267b1d75b64e73356a
mhyuan <dsz9258@163.com>
2018-11-05 16:41:57 +0800

    积分兑换页

--- src/pages/pointsExchange/index.js
+++ src/pages/pointsExchange/index.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import Header from '../../components/header';
+import { getPersonalData } from '../../store/actionCreators';
 import { 
     ExchangeWrap,
     TopContent,
@@ -9,7 +10,21 @@ import {
 
 
 class PointsExchange extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: ''
+        }
+        this.selectedPoints = '';
+        this.selectAllPointsHandle = this.selectAllPointsHandle.bind(this);
+        this.pointInputChange = this.pointInputChange.bind(this);
+    }
     render() {
+        let { personal_info, exchangeHandle } = this.props;
+        let { inputValue } = this.state;
+        
+        let info = personal_info ? personal_info.toJS() : {};
+        
         return (
             <ExchangeWrap>
                 <Header>
@@ -20,17 +35,79 @@ class PointsExchange extends PureComponent {
                 <TopContent>
                     <div className="top_wrap">
                         <h3 className="total_integral">总积分</h3>
-                        {/* <span className="integral">{info.points}</span>
-                        <span className="integral_level">等级: {info.level}</span> */}
+                        <span className="integral">{info.points}</span>
+                        <span className="integral_level">等级: {info.level}</span>
                     </div>
                 </TopContent>
                 <ExchangeMain>
-                    PointsExchange
+                    <div className="exchange_content">
+                        <label className="input_label" htmlFor="exchange">积分兑换:
+                            <input value={ inputValue } onChange={ this.pointInputChange } className="point_input" placeholder="请输入兑换积分" name="exchange" />
+                            {/* <i className="clear_all">X</i> */}
+                        </label>
+                        <button onClick={() => this.selectAllPointsHandle(info.points)} className="exchange_all_button">全部兑换</button>
+                    </div>
+                    <div className="exchange_explain">
+                        <h3 className="explan_title">兑换说明</h3>
+                        {
+                            info.exchange_explain
+                            ? info.exchange_explain.map((item, index) => {
+                                return (
+                                    <span key={index} className="explan_item">{item}</span>
+                                )
+                            })
+                            : null
+                        }
+                    </div>
+                    <div className="exchange_route">
+                        <img className="exchange_qr" src={info.qr_url} alt="客服二维码" />
+                        <button className="exchange_button" onClick={exchangeHandle}>兑换</button>
+                    </div>
                 </ExchangeMain>
             </ExchangeWrap>
         )
     }
+    componentDidMount() {
+        this.props.getData();
+    }
+    pointInputChange(e) {
+        let newValue  = e.target.value;
+        if(/\D/.test(newValue)) return console.log(`please input number , ${newValue}`);
+        this.setState({
+            inputValue: newValue
+        });
 
+        this.selectedPoints = this.state.inputValue;
+        console.log('this.selectedPoints', this.selectedPoints);
+    }
+    selectAllPointsHandle(points) {
+        if(points) {
+            this.selectedPoints = points;
+            this.setState((state) => ({
+                inputValue: points
+            }));
+            console.log('this.selectedPoints', this.selectedPoints);
+            
+        }
+    }
+    
 }
 
-export default PointsExchange;
+const mapState = (state) => ({
+    personal_info: state.get('personal_info'),
+})
+
+const mapDispatch = (dispatch) => ({
+    getData() {        
+        if(!(this.personal_info && this.personal_info.size > 0)) {
+            let action = getPersonalData();
+            dispatch(action);
+        }
+    },
+    exchangeHandle() {
+        console.log('exchangeHandle event happened!');
+        
+    }
+})
+
+export default connect(mapState, mapDispatch)(PointsExchange);
--- src/pages/pointsExchange/style.js
+++ src/pages/pointsExchange/style.js
@@ -5,6 +5,7 @@ export const ExchangeWrap = Styled.div`
 `
 export const TopContent = Styled.div`
     display: block;
+    margin-top: 0.8rem;
     padding: 0 auto 0.4rem;
     overflow: hidden;
     box-sizing: border-box;
@@ -50,5 +51,76 @@ export const TopContent = Styled.div`
 export const ExchangeMain = Styled.div`
     dosplay: block;
     margin-top: 0.8rem;
-    padding: 0.2rem 0.2rem;
+    box-sizing: border-box;
+    .exchange_content {
+        display: inline-block;
+        width: 100%;
+        padding: 0.2rem 0.2rem;
+        line-height: 0.8rem;
+        border-top: 0.01rem solid #999;
+        border-bottom: 0.01rem solid #999;
+        box-sizing: border-box;
+        .input_label {
+            position: relative;
+            display: inline-block;
+            width: calc(100% - 1.6rem);
+            .point_input {
+                width: calc(100% - 2rem);
+                height: 0.4rem;
+                margin-left: 0.05rem;
+                padding-left: 0.1rem;
+                border: 0.01rem solid #999;
+                border-radius: 0.07rem;
+            }
+        }
+        .exchange_all_button {
+            display: inline-block;
+            width: 1.2rem;
+            border: 0.01rem solid #ccc;
+            background-color: #fff;
+            border-radius: 0.06rem;
+        }
+    }
+    .exchange_explain {
+        display: block;
+        padding: 0.2rem 0.2rem;
+        box-sizing: border-box;
+        .explan_title {
+            display: inline-block;
+            width: 100%;
+            height: 0.5rem;
+            line-height: 0.5rem;
+        }
+        .explan_item {
+            display: inline-block;
+            width: 100%;
+            line-height: 0.5rem;
+        }
+    }
+    .exchange_route {
+        display: block;
+        width: 100%;
+        text-align: center;
+        padding: 0.3rem 0.2rem;
+        box-sizing: border-box;
+        .exchange_qr {
+            display: block;
+            width: 3rem;
+            margin: 0.2rem auto 0.3rem;
+        }
+        .exchange_button{
+            display: inline-block;
+            width: 3rem;
+            height: 0.5rem;
+            line-height: 0.5rem;
+            margin: 0.2rem auto;
+            border: 0.01rem solid #999;
+            border-radius: 0.08rem;
+            text-align: center;
+            background-color: #fff;
+            font-size: 0.28rem;
+            font-weight: bold;
+        }
+    }
 `
+
