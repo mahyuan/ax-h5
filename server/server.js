55ddc2b19d14a3d06f71d4f1234eee4670506ab1
mhyuan <dsz9258@163.com>
2018-12-22 12:38:46 +0800

    Add: add apply page and server add post request

--- server/server.js
+++ server/server.js
@@ -1,59 +1,88 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let fs = require('fs');
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 const PORT = 4003;
 const routes = [
     {
         name: "home_data",
-        pype: "get",
+        type: "get",
         path: "/api/home_data"
     },
     {
         name: "project_list",
-        pype: "get",
+        type: "get",
         path: "/api/get_projet_list"
     },
     {
         name: "project_list",
-        pype: "get",
+        type: "get",
         path: '/api/get_detail/:pid'
     },
     {
         name: "activity_info",
-        pype: "get",
+        type: "get",
         path: "/api/activity/list"
     },
     {
         name: "activity_info",
-        pype: "get",
+        type: "get",
         path: "/api/activity/detail/:aid"
     },
     {
         name: "persional",
-        pype: "get",
+        type: "get",
         path: "/api/personal"
-    }
+	},
+	{
+		name: "putApply",
+		type: "post",
+		path: "/api/persion/apply/submit"
+	}
 ]
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'origin, x-requested-with, content-type, accept');
+	next();
+})
 routes.forEach((route, index) => {
-    let { name, path } = route;
-    
-    app.get(path, function(req, res) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
+	let { name, path } = route;
+	let type = route.type;
 
-        if(req.method === 'OPTIONS') {
-            res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,DELETE');
-            res.setHeader('Access-Control-Allow-Headers', 'origin, x-requested-with, content-type, accept');
-            res.setHeader('Content-Type', 'application/json;charset=utf-8');
-            res.setHeader('Access-Control-Allow-Credentials', true);
-        }
-         
-        fs.readFile('./mook/' + name + '.json', function(err, data) {
-            if(err) throw err;
-            console.log(`get request from ${path}, response: ${data}`);
-            
-            res.json(JSON.parse(data));
-        })
-    })
+	if (type === 'get') {
+		app.get(path, function(req, res) {
+			// console.log('req', req);
+			// console.log('res', res);
+			if(req.method === 'OPTIONS') {
+				res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,DELETE');
+				res.setHeader('Content-Type', 'application/json;charset=utf-8');
+				res.setHeader('Access-Control-Allow-Credentials', true);
+			}
+
+			let { query } = req;
+			console.log('get')
+			fs.readFile('./mook/' + name + '.json', function(err, data) {
+				if(err) throw err;
+				// console.log(`get request from ${path}, response: ${data}`);
+
+				res.json(JSON.parse(data));
+			})
+		})
+	}
+
+	if (type === 'post') {
+		app.post(path, function(req, res) {
+			let params = req.body;
+			console.log('post data', params)
+			res.send({
+				"code": 0,
+				"msg": ''
+			})
+		})
+	}
+
 })
 
 app.listen(PORT, () => {
