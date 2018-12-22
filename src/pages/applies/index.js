55fb72d5d0e8e3057fb19a4509668304cde310bf
mhyuan <dsz9258@163.com>
2018-12-22 12:38:46 +0800

    Add: add apply page and server add post request

--- src/pages/applies/index.js
+++ src/pages/applies/index.js
@@ -1,53 +1,84 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import Header from '../../components/header';
-// import { getPersonalData } from '../../store/actionCreators';
+import { /* getPersonalData, */ submitApply } from '../../store/actionCreators';
 import {
     MyAppliedWrap,
     AppliedContent,
 } from './style';
 
 class Applies extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			point: '',
+			userName: '',
+			school: '',
+			mobile: '',
+			message: ''
+		}
+		this.changeInputValue = this.changeInputValue.bind(this)
+	}
     componentDidMount() {
     }
-    
+
     render() {
-        let { personal_info } = this.props;
-        let explain = (personal_info && personal_info.size > 0) ? personal_info.toJS().points_explain : {};
-        console.log('explain' , explain);
-        
+		let { point, userName, school, mobile, message } = this.state;
+		const { submitApplayHandle } = this.props;
         return (
             <MyAppliedWrap>
                 <Header>
                     {
                         <span show_back="show">我的报名</span>
                     }
-                </Header>     
-                <AppliedContent> 
-  
+                </Header>
+                <AppliedContent>
+					<h3 className="project">项目名称：<em>英国证券交易所</em></h3>
+					<p className="funds">$40000</p>
+					<label className="labelInsert" htmlFor="point">
+						<input name="point" onChange={($event) => this.changeInputValue('point', $event)} value={point} placeholder="使用积分"/>
+					</label>
+					<label className="labelInsert" htmlFor="userName">
+						<input name="userName" onChange={($event) => this.changeInputValue('userName', $event)} value={userName} placeholder="姓名"/>
+					</label>
+					<label className="labelInsert" htmlFor="school">
+						<input name="school" onChange={($event) => this.changeInputValue('school', $event)} value={school} placeholder="学校"/>
+					</label>
+					<label className="labelInsert" htmlFor="mobile">
+						<input type="number" name="mobile" onChange={($event) => this.changeInputValue('mobile', $event)} value={mobile}  placeholder="联系方式"/>
+					</label>
+					<label className="labelInsert" htmlFor="message">
+						<input onChange={($event) => this.changeInputValue('message', $event)} value={message} rows="3" name="message" placeholder="留言"/>
+					</label>
+					<button className="submitApply" onClick={($event) => submitApplayHandle(this.state, $event)}>报名</button>
                 </AppliedContent>
             </MyAppliedWrap>
         )
-    }
+	}
+	changeInputValue(name, $event) {
+		let value = $event.target.value
+		if (name === 'mobile') {
+			value = value.replace(/\D/g, '');
+			value.length >= 11 && (value = value.substring(0, 11));
+		}
 
+		this.setState(() => ({
+			[name]: value
+		}))
+	}
 }
 
 const mapState = (state) => ({
-    personal_info: state.get('personal_info'),
+	// personal_info: state.get('personal_info'),
 })
 
 const mapDispatch = (dispatch) => ({
-    // getProfile(explain) {
-    //     if(!(explain && explain.size > 0)) {
-    //         let action = getPersonalData();
-    //         dispatch(action);
-    //         console.log('1');
-            
-    //     } else {
-    //         console.log('2, MyApplied');
-            
-    //     }
-    // }
+	submitApplayHandle(state, $event) {
+		console.log('state', state);
+		console.log('$evetn', $event)
+		const action = submitApply(state);
+		dispatch(action);
+	}
 })
 
 export default connect(mapState, mapDispatch)(Applies);
